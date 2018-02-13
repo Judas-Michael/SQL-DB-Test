c80665769946b77bb74aeea11558018e23d66582
JL <lollypopsandrainbows@gmail.com>
2018-02-13 14:49:50 -0600

    Added notes

--- db_test.py
+++ db_test.py
@@ -1,12 +1,14 @@
-import sqlite3
-import unittest
+import sqlite3 #imports DB ability
+import unittest #imports ability to test
+from unittest.mock import patch #imports ability to patch a mock
 
-  @mock.patch(sqlite3) 
-def test_main(self, mock_db):
+
+  @mock.patch(sqlite3)  #tells program to create a mock object to act in place of sqlite3
+def test_main(self, mock_db): #a test function that talks to itself and the mock_db sqlite3)
   
 	##TODO Add [] to db so that assertion passes
-    sqlite_db = sqlite3.connect('database.sqlite')
-	assert(sqlite_db[0] == 'Two')
+    sqlite_db = sqlite3.connect('database.sqlite')#creates db that connects to mock db
+	assert(sqlite_db[0] == 'Two') #asserts that the fake index is identical
 	
   
   
